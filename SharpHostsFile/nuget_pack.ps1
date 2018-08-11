3fbed121220241a945277f7dc39209ef9277c70e
Nate Shoffner <nate.shoffner@gmail.com>
2018-08-11 08:27:02 -0400

    Attemp #3

--- SharpHostsFile/nuget_pack.ps1
+++ SharpHostsFile/nuget_pack.ps1
@@ -1,2 +1,2 @@
-$path = $env:APPVEYOR_BUILD_FOLDER + "\SharpHostsFile.nuspec"
+$path = $env:APPVEYOR_BUILD_FOLDER + "\SharpHostsFile\SharpHostsFile.nuspec"
 nuget pack $path -Version $env:APPVEYOR_BUILD_VERSION
